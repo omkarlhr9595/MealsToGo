50f52f893fb92d701d285ac0b4f2c0f9a1405b6f
Omkar Lohar <omkarlohar91@gmail.com>
2021-12-13 19:09:14 +0530

    Searching for Restaurant (13-12-2021 Omkar Lohar)

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { Searchbar } from "react-native-paper";
 
@@ -11,6 +11,11 @@ const SearchContainer = styled.View`
 const Search = () => {
   const { keyword,search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
+
+  useEffect(() => {
+    search(searchKeyword)
+  }, [])
+
   return (
     <SearchContainer>
       <Searchbar
