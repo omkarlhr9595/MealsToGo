6fbf30b92efbbb3b27a74817d7426970e9c02261
Omkar Lohar <omkarlohar91@gmail.com>
2021-12-20 15:10:27 +0530

    Favorites Bar Added 3:10 PM Omkar Lohar

--- src/components/favorites/favorites-bar.component.js
+++ src/components/favorites/favorites-bar.component.js
@@ -1,7 +1,43 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { BottomNavigation } from "react-native-paper";
+import styled from "styled-components";
+import { CompactRestaurantInfo } from "../restaurant/component-restaurant-info.component";
+import { Spacer } from "../spacer/spacer.component";
+import { Text } from "../typography/text.component";
 
-export const FavoritesBar = () =>{
-    return<Text>FavBar</Text>
+const FavoritesWrapper = styled.View`
+  padding: 10px;
+`;
 
-}
+export const FavoritesBar = ({ favorites, onNavigate }) => {
+  if (!favorites.length) {
+    return null;
+  }
+  return (
+    <FavoritesWrapper>
+      <Spacer varient="left.large">
+        <Text varient="caption">Favorites</Text>
+      </Spacer>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator>
+        {favorites.map((restaurant) => {
+          const key = restaurant.name.split(" ").join("");
+          return (
+            <Spacer key={key} position="left" size="medium">
+              <TouchableOpacity
+                onPress={() =>
+                  onNavigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavoritesWrapper>
+  );
+};
