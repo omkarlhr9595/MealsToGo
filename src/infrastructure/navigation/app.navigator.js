1e324fb35ae81ddd522df152820d491451668dbe
Omkar Lohar <omkarlohar91@gmail.com>
2021-12-22 21:13:21 +0530

    Settings Screen Added

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,17 +1,15 @@
-import React, { useContext } from "react";
-import { Text, Button, View } from "react-native";
+import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { RestaurantsNavigator } from "./restaurants.navigator";
+import { SettingsNavigator } from "./settings.navigator";
 import { theme } from "../theme";
 import { MapScreen } from "../../features/map/screens/map.screen";
 import { RestaurantsContextProvider } from "../../services/restaurants/restaurants.context";
 import { LocationContextProvider } from "../../services/location/location.context";
 import { FavoritesContextProvider } from "../../services/favorites/favorites.context";
 const Tab = createBottomTabNavigator();
-
-import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 const TAB_ICON = {
   Restaurants: "md-restaurant",
@@ -31,22 +29,6 @@ const createScreenOptions = ({ route }) => {
   };
 };
 
-const Settings = () => {
-  const { onLogout } = useContext(AuthenticationContext);
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Settings</Text>
-      <Button title="LogOut" onPress={() => onLogout()} />
-    </View>
-  );
-};
-
 export const AppNavigator = () => (
   <>
     <FavoritesContextProvider>
@@ -55,7 +37,7 @@ export const AppNavigator = () => (
           <Tab.Navigator screenOptions={createScreenOptions}>
             <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
             <Tab.Screen name="Map" component={MapScreen} />
-            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="Settings" component={SettingsNavigator} />
           </Tab.Navigator>
         </RestaurantsContextProvider>
       </LocationContextProvider>
